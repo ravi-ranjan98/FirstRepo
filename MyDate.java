3c315b0a438e8e78b8834e46b6f9f9f6afd69ca7
ravi-ranjan98 <73465868+ravi-ranjan98@users.noreply.github.com>
2020-10-30 14:36:05 +0530

    First Commit

---
+++ MyDate.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class MyDate {
+
+    private int day, month,year;
+
+    MyDate(){
+        this.day=20;
+        this.month=11;
+        this.year=2020;
+    }
+
+    MyDate( int day , int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    int getDay(){
+        return day;
+    }
+    int getMonth(){
+        return month;
+    }
+    int getYear(){
+        return year;
+    }
+
+    void setDay(int day){
+        this.day = day;
+
+    }
+    void setMonth(int month){
+        this.month = month;
+
+    }
+
+    void setYear(int year){
+        this.year = year;
+    }
+
+    void display(){
+        System.out.println(day+"/"+month+"/"+year);
+    }
+
+    void set(int day , int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+    
+    // static void show(){
+    //     System.out.println("in static method");
+    // }
+
+    public static void main(String args[]){
+       MyDate d1 = new MyDate();
+       System.out.println("Default constructor");
+       d1.display();
+       MyDate d2 = new MyDate(20,10,2020);
+       System.out.println("Parameterized constructor");
+       d2.display();
+       MyDate d3 = new MyDate();
+       Scanner in = new Scanner(System.in);
+       System.out.println("Enter Day");
+       int day = in.nextInt();
+       System.out.println("Enter Month");
+       int month = in.nextInt();
+       System.out.println("Enter Year");
+       int year = in.nextInt();
+       d3.set(day,month,year);
+       System.out.println("User Input");
+       d3.display();
+
+    }
+}
